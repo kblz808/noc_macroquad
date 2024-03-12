5b9d5f16316d97d6f7dbd0bd56b9d8739ab1f1f6
kaleab <808kaleabkzin@gmail.com>
2024-03-12 22:49:27 +0300

    chapter 1 example 4

---
+++ examples/chapter1/chapter1_4.rs
@@ -0,0 +1,42 @@
+use macroquad::{prelude::*, window};
+
+fn setup() {
+    window::request_new_screen_size(640.0, 240.0);
+}
+
+#[macroquad::main("Multiplying a Vector")]
+async fn main() {
+    setup();
+
+    let stroke_width = 5.0;
+
+    loop {
+        clear_background(WHITE);
+
+        let mut mouse = vec2(mouse_position().0, mouse_position().1);
+        let center = vec2(screen_width() / 2.0, screen_height() / 2.0);
+        mouse = mouse - center;
+
+        let stroke_color = Color::new(200.0 / 255.0, 200.0 / 255.0, 200.0 / 255.0, 1.0);
+        draw_line(
+            center.x,
+            center.y,
+            center.x + mouse.x,
+            center.y + mouse.y,
+            stroke_width,
+            stroke_color,
+        );
+
+        mouse = mouse * 0.5;
+        draw_line(
+            center.x,
+            center.y,
+            center.x + mouse.x,
+            center.y + mouse.y,
+            stroke_width,
+            BLACK,
+        );
+
+        next_frame().await
+    }
+}
