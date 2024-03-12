68ea9bc3592a47bbce7971efee6355b3629455e7
kaleab <808kaleabkzin@gmail.com>
2024-03-12 21:58:09 +0300

    chapter 1 example 1

---
+++ examples/chapter1/chapter1_1.rs
@@ -0,0 +1,35 @@
+use macroquad::{prelude::*, window};
+
+fn setup() {
+    window::request_new_screen_size(640.0, 240.0);
+}
+
+#[macroquad::main("Bouncing Ball with No Vectors")]
+async fn main() {
+    setup();
+
+    let mut x = 100.0;
+    let mut y = 100.0;
+    let mut xspeed = 2.5;
+    let mut yspeed = 2.0;
+
+    loop {
+        clear_background(WHITE);
+
+        x = x + xspeed;
+        y = y + yspeed;
+
+        if x > screen_width() || x < 0.0 {
+            xspeed = xspeed * -1.0;
+        }
+        if y > screen_height() || y < 0.0 {
+            yspeed = yspeed * -1.0;
+        }
+
+        let color = Color::new(127.0 / 255.0, 127.0 / 255.0, 127.0 / 255.0, 1.0);
+
+        draw_circle(x, y, 48.0, color);
+
+        next_frame().await
+    }
+}
