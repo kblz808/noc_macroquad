9d675a92cec0c7c67d18dac34689237112b1911c
kaleab <808kaleabkzin@gmail.com>
2024-03-13 10:18:43 +0300

    chapter 1 example 7

---
+++ examples/chapter1/chapter1_7.rs
@@ -0,0 +1,68 @@
+use std::ops::Add;
+
+use macroquad::{prelude::*, window};
+
+fn setup() {
+    // seed the random number with unique data
+    rand::srand(macroquad::miniquad::date::now() as _);
+
+    window::request_new_screen_size(640.0, 240.0);
+}
+
+struct Mover {
+    position: Vec2,
+    velocity: Vec2,
+}
+
+impl Mover {
+    // like js constructor
+    fn new() -> Self {
+        return Self {
+            position: vec2(
+                rand::gen_range(0.0, screen_width()),
+                rand::gen_range(0.0, screen_height()),
+            ),
+            velocity: vec2(rand::gen_range(-2.0, 2.0), rand::gen_range(-2.0, 2.0)),
+        };
+    }
+
+    fn update(&mut self) {
+        self.position = self.position.add(self.velocity);
+    }
+
+    fn show(&self) {
+        let color = Color::from_rgba(127, 127, 127, 255);
+        draw_circle(self.position.x, self.position.y, 48.0, color);
+    }
+
+    fn check_edges(&mut self) {
+        if self.position.x > screen_width() {
+            self.position.x = 0.0;
+        } else if self.position.x < 0.0 {
+            self.position.x = screen_width();
+        }
+
+        if self.position.y > screen_height() {
+            self.position.y = 0.0;
+        } else if self.position.y < 0.0 {
+            self.position.y = screen_height();
+        }
+    }
+}
+
+#[macroquad::main("Motion 101 (Velocity)")]
+async fn main() {
+    setup();
+
+    let mut mover = Mover::new();
+
+    loop {
+        clear_background(WHITE);
+
+        mover.update();
+        mover.check_edges();
+        mover.show();
+
+        next_frame().await
+    }
+}
